7c9c8f4678c0f06ddf8e66b191b6a9aa7df3a9de
Saurabh1997 <patilsaurabh.2013@gmail.com>
2020-04-09 01:46:41 +0530

    final

--- src/config/db.js
+++ src/config/db.js
@@ -5,4 +5,7 @@ const config = getConfig(process.env.NODE_ENV); //will get node env from running
 //mongoose.Promise = global.Promise
 export const connect = () =>
   // mongoose.connect("mongodb://localhost/music_api", { useNewUrlParser: true });
-  mongoose.connect(config.MONGO_URI, { useUnifiedTopology: true });
+  mongoose.connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
